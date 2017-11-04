c669f3dc065ba8c4a37a9bde01111ce29c01d308
GerganaStanilova <stanilova.gergana@gmail.com>
2017-11-04 13:52:08 +0100

    Bug fixing von FrequentWords

--- praktikum1/src/com/example/albi/praktikum1/FrequentWords.java
+++ praktikum1/src/com/example/albi/praktikum1/FrequentWords.java
@@ -1,11 +1,10 @@
 package com.example.albi.praktikum1;
 
 public class FrequentWords {
-    public static int patternCount(){
-        String text = "GCGCG";
-        String pattern = "GCG";
+    public static int patternCount(String text, String pattern){
+
         int count = 0;
-        for(i=0; i <= (text.length() - pattern.length()); i++){
+        for(int i=0; i <= (text.length() - pattern.length()); i++){
             if ((text.substring(i, pattern.length())).equals(pattern)){
                 count++;
             }
