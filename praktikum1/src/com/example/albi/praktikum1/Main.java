97547e6daa63f1b3f7ebad8cdcdf23e5634c8d77
GerganaStanilova <stanilova.gergana@gmail.com>
2017-11-04 15:23:26 +0100

    Funktion FrequentWords ausprobieren

--- praktikum1/src/com/example/albi/praktikum1/Main.java
+++ praktikum1/src/com/example/albi/praktikum1/Main.java
@@ -5,9 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //Hallo Welt
-        String text = "GCGCG";
-        String pattern = "GCG";
-        int c = FrequentWords.patternCount(text, pattern);
-        System.out.println(c);
+        String text = "ACGTTGCATGTCGCATGATGCATGAGAGCT";
+        int k = 4;
+        for (String item : FrequentWords.frequentWords(text,k)) {
+            System.out.println(item);
+        }
     }
 }
