300bf6b80ab2979d0baf75f4c5c15e51423faf31
GerganaStanilova <stanilova.gergana@gmail.com>
2017-11-04 17:39:01 +0100

    fasterFrequentWords implementieren

--- praktikum1/src/com/example/albi/praktikum1/FastFrequentWords.java
+++ praktikum1/src/com/example/albi/praktikum1/FastFrequentWords.java
@@ -1,5 +1,6 @@
 package com.example.albi.praktikum1;
-
+import java.util.HashSet;
+import java.util.Set;
 public class FastFrequentWords {
     public static int symbolToNumber(String symbol){
         switch (symbol){
@@ -46,5 +47,37 @@ public class FastFrequentWords {
         String symbol = numberToSymbol(r);
         String prefixPattern = numberToPattern(prefIndex, k - 1);
         return prefixPattern + symbol;
+    }
+    public static int[] computingFrequencies(String text, int k){
+        int len = (int) (Math.pow(4,k));
+        int[] frequencyArray = new int[len];
+        for(int i = 0; i < len; i++){
+            frequencyArray[i] = 0;
+        }
+        for(int i = 0; i <= (text.length() - k); i++){
+            String pattern = text.substring(i, i + k);
+            int j = patternToNumber(pattern);
+            frequencyArray[j] = frequencyArray[j] + 1;
+        }
+        return frequencyArray;
+    }
+
+    public static Set <String> fasterFrequentwords(String text, int k) {
+        Set<String> frequentPatterns = new HashSet<String>();
+        int[] frequencyArray = computingFrequencies(text,k);
+        int maxCount = 0;
+        for(int i = 0; i < frequencyArray.length; i++){
+            if(frequencyArray[i] > maxCount){
+                maxCount = frequencyArray[i];
+            }
+        }
+        int len = (int) (Math.pow(4,k));
+        for(int i = 0; i < len; i++){
+            if (frequencyArray[i] == maxCount){
+                String pattern = numberToPattern(i, k);
+                frequentPatterns.add(pattern);
+            }
+        }
+        return frequentPatterns;
     }
 }
