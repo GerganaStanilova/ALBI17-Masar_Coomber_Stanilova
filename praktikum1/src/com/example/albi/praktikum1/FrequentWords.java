232a73f5a594ad96780b7fdffaed9911173f5140
GerganaStanilova <stanilova.gergana@gmail.com>
2017-11-04 14:25:03 +0100

    Kommentare zu PatternCount

--- praktikum1/src/com/example/albi/praktikum1/FrequentWords.java
+++ praktikum1/src/com/example/albi/praktikum1/FrequentWords.java
@@ -1,15 +1,14 @@
 package com.example.albi.praktikum1;
 
 public class FrequentWords {
-    public static int patternCount(String text, String pattern){
+    public static int patternCount(String text, String pattern){ //die Fkt kriegt als Parameter text und pattern
 
-        int count = 0;
-        for(int i=0; i <= (text.length() - pattern.length()); i++){
-            if ((text.substring(i, pattern.length())).equals(pattern)){
-                count++;
+        int count = 0; //den Counter auf 0 setzen
+        for(int i=0; i <= (text.length() - pattern.length()); i++){ //geht alle nötigen Positionen im Text durch
+            if ((text.substring(i, i + pattern.length())).equals(pattern)){ // prüft, ob das Pattern dem Substring entpricht
+                count++; //falls ja, den Counter mit 1 erhöhen
             }
         }
-        return count;
-
+        return count; // die Anzahl des Vorkommens des Patterns im Text
     }
 }
