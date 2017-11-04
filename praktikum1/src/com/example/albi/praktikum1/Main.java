7932d877f836b29d932d2a5abe8898164febb16f
GerganaStanilova <stanilova.gergana@gmail.com>
2017-11-04 14:25:51 +0100

    PatternCount mit beliebigen Text und Pattern ausprobieren

--- praktikum1/src/com/example/albi/praktikum1/Main.java
+++ praktikum1/src/com/example/albi/praktikum1/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //Hallo Welt
+        String text = "GCGCG";
+        String pattern = "GCG";
+        int c = FrequentWords.patternCount(text, pattern);
+        System.out.println(c);
     }
 }
