c59ba4ff30b3de5c97c2c617b848597d0b8b8efb
GerganaStanilova <stanilova.gergana@gmail.com>
2017-11-04 17:39:35 +0100

    fasterFrequentWords ausprobieren

--- praktikum1/src/com/example/albi/praktikum1/Main.java
+++ praktikum1/src/com/example/albi/praktikum1/Main.java
@@ -3,13 +3,22 @@ package com.example.albi.praktikum1;
 public class Main {
 
     public static void main(String[] args) {
-        String text = "ACGTTGCATGTCGCATGATGCATGAGAGCT";
+        /*String text = "ACGTTGCATGTCGCATGATGCATGAGAGCT";
         int k = 4;
         for (String item : FrequentWords.frequentWords(text,k)) { //jedes Element im Set frequentPatterns ausgeben
             System.out.println(item);
         }
-        String pattern = "AGT";
-        System.out.println(FastFrequentWords.patternToNumber(pattern));
-        System.out.println(FastFrequentWords.numberToPattern(11,3));
+        //String pattern = "AGT";
+        //System.out.println(FastFrequentWords.patternToNumber(pattern));
+        //System.out.println(FastFrequentWords.numberToPattern(11,3));
+
+        for (int item : FastFrequentWords.computingFrequencies("AAGCAAAGGTGGG",2)) { //jedes Element im Set frequentPatterns ausgeben
+            System.out.println(item);
+        }*/
+        String text = "ACGTTGCATGTCGCATGATGCATGAGAGCT";
+        int k = 4;
+        for (String item : FastFrequentWords.fasterFrequentwords(text,k)) { //jedes Element im Set frequentPatterns ausgeben
+            System.out.println(item);
+        }
     }
 }
